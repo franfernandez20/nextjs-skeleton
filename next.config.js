14f077a8a34d8f747c412208c425fc8912a4fcf9
fcofdez <franfernandez20@gmail.com>
2021-05-31 23:29:49 +0200

    (Deploying on Vercel | 3)

--- next.config.js
+++ next.config.js
@@ -4,7 +4,6 @@ const runtimeCaching = require('next-pwa/cache')
 module.exports = withPWA({
   pwa: {
     dest: 'public',
-    // disable: process.env.NODE_ENV === 'development',
     runtimeCaching
   }
 })
--- pages/_app.js
+++ pages/_app.js
@@ -1,38 +1,10 @@
 import '../styles/globals.css'
-import Head from 'next/head'
 import { ChakraProvider } from '@chakra-ui/react'
 import { theme } from '@chakra/theme'
 
 function MyApp ({ Component, pageProps }) {
   return (
     <>
-      <Head>
-        <meta charSet="utf-8" />
-        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        <meta
-          name="viewport"
-          content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
-        />
-        <meta name="description" content="Description" />
-        <meta name="keywords" content="Keywords" />
-        <title>Next.js PWA Example</title>
-
-        <link rel="manifest" href="/manifest.json" />
-        <link
-          href="/icons/favicon-16x16.png"
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-        />
-        <link
-          href="/icons/favicon-32x32.png"
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-        />
-        <link rel="apple-touch-icon" href="/apple-icon.png"></link>
-        <meta name="theme-color" content="#317EFB" />
-      </Head>
       <ChakraProvider theme={theme}>
         <Component {...pageProps} />
       </ChakraProvider>
